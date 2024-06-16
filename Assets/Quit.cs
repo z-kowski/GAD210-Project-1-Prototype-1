afb08a5f4c841cd8df38d117947b0f097c5423c0
Keegan <z-kowski@hotmail.com>
2024-06-16 21:31:12 +0800

    Added quit app. script

---
+++ Assets/Quit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.OpenURL("https://forms.gle/QtgsorsTwBgXyShH6");
+            Application.Quit();
+        }
+    }
+}
